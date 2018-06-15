121b47bd8cda5f029eeca144734909995303895f
wengshifei <wengshifei@qq.com>
2018-06-15 12:17:39 +0000

    Init CTA
    git-tfs-id: [https://cmroot.visualstudio.com]$/CTA;C11

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"x + y = {x} + {y}");
+            Console.ReadLine();
+        }
+    }
+}
